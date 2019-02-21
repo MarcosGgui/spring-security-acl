bbbb2e364c5e823d2b160a4a5336b12e356773cf
Marcos Gui <marcos.gui@spsoft-cn.com>
2019-02-21 18:12:09 +0800

    [IMP] Improve the request to create the data of  acl control table

--- src/main/java/com/spring/security/acl/repository/NoticeMessageRepository.java
+++ src/main/java/com/spring/security/acl/repository/NoticeMessageRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 
@@ -15,15 +16,15 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NoticeMessageRepository extends JpaRepository<NoticeMessage, Long>{
 
-//    @Override
-//    @PostFilter("hasPermission(filterObject, 'READ')")
-//    List<NoticeMessage> findAll();
+    @Override
+    @PostFilter("hasPermission(filterObject, 'READ')")
+    List<NoticeMessage> findAll();
 //
 //    @PostAuthorize("hasPermission(returnObject, 'READ')")
 //    NoticeMessage findById(Integer id);
 //
-//    @Override
-//    @SuppressWarnings("unchecked")
-////    @PostFilter("hasPermission(filterObject, 'WRITE')")
-//    NoticeMessage save(@Param("noticeMessage") NoticeMessage noticeMessage);
+    @Override
+    @SuppressWarnings("unchecked")
+    @PreAuthorize("hasPermission(#noticeMessage, 'WRITE')")
+    NoticeMessage save(@Param("noticeMessage") NoticeMessage noticeMessage);
 }
--- src/main/webapp/app/home/access-control/access-control.component.ts
+++ src/main/webapp/app/home/access-control/access-control.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { User, UserService } from 'app/core';
 import { AclSidService } from 'app/entities/acl-sid';
 import { AclClassService } from 'app/entities/acl-class';
 import { AclObjectIdentityService } from 'app/entities/acl-object-identity';
 import { AclEntryService } from 'app/entities/acl-entry';
 import { AclSid } from 'app/shared/model/acl-sid.model';
-import { HttpResponse } from '@angular/common/http';
+import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
 import { AclClass } from 'app/shared/model/acl-class.model';
+import { AclObjectIdentity } from 'app/shared/model/acl-object-identity.model';
+import { AclEntry } from 'app/shared/model/acl-entry.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { JhiAlertService } from 'ng-jhipster';
 
 @Component({
     selector: 'jhi-access-control',
@@ -18,13 +22,20 @@ export class AccessControlComponent implements OnInit {
 
     private accessUser: User;
     private className: string;
+    private access: string;
+    private aclSid: AclSid = new AclSid();
+    private aclClass: AclClass = new AclClass();
+    private aclObjectIdentity: AclObjectIdentity = new AclObjectIdentity();
+    private aclEntry: AclEntry = new AclEntry();
 
     constructor(
+        private jhiAlertService: JhiAlertService,
         private userService: UserService,
         private aclClassService: AclClassService,
         private aclSidService: AclSidService,
         private aclObjectIdentityService: AclObjectIdentityService,
-        private aclEntryService: AclEntryService
+        private aclEntryService: AclEntryService,
+        private modalService: NgbModal
     ) {}
 
     ngOnInit() {
@@ -38,17 +49,57 @@ export class AccessControlComponent implements OnInit {
     }
 
     addSid() {
-        const aclSid = new AclSid();
-        aclSid.principal = 1;
-        aclSid.sid = this.accessUser.login;
-        console.log(aclSid);
-        this.aclSidService.create(aclSid).subscribe(res => {});
+        this.aclSid.principal = 1;
+        this.aclSid.sid = this.accessUser.login;
+        this.aclSidService.create(this.aclSid).subscribe((res: HttpResponse<any>) => {
+            this.aclSid = res.body;
+            this.addSidClass();
+        });
     }
 
     addSidClass() {
-        const aclClass = new AclClass();
-        aclClass.classPath = this.className;
-        console.log(aclClass);
-        this.aclClassService.create(aclClass).subscribe(res => {});
+        this.aclClass.classPath = this.className;
+
+        this.aclClassService.create(this.aclClass).subscribe(
+            (res: HttpResponse<any>) => {
+                this.aclClass = res.body;
+                this.addObjectIdIdentity();
+            },
+            (res: HttpErrorResponse) => this.onError(res.error)
+        );
+    }
+
+    addObjectIdIdentity() {
+        this.aclObjectIdentity.objectIdIdentity = '1';
+        this.aclObjectIdentity.entriesInheriting = 1;
+        this.aclObjectIdentity.objectIdClass = this.aclClass.id;
+        this.aclObjectIdentity.ownerSid = this.aclSid.id;
+
+        this.aclObjectIdentityService.create(this.aclObjectIdentity).subscribe(
+            (res: HttpResponse<AclObjectIdentity>) => {
+                this.aclObjectIdentity = res.body;
+                this.addAclEntry();
+            },
+            (res: HttpErrorResponse) => this.onError(res.error)
+        );
+    }
+
+    addAclEntry() {
+        this.aclEntry.mask = Number.parseInt(this.access);
+        this.aclEntry.aceOrder = 1;
+        this.aclEntry.granting = 1;
+        this.aclEntry.auditFailure = 1;
+        this.aclEntry.auditSuccess = 1;
+        this.aclEntry.sid = this.aclSid.id;
+        this.aclEntry.aclObjectIdentity = this.aclObjectIdentity.id;
+        this.aclEntryService.create(this.aclEntry).subscribe(res => {}, (res: HttpErrorResponse) => this.onError(res.error));
+    }
+
+    addAccess() {
+        this.addSid();
+    }
+
+    private onError(errorMessage: string) {
+        this.jhiAlertService.error(errorMessage, null, null);
     }
 }
